d262ef065babd4c43eae83d6e4afd6c30135424f
CristinaMarilena <cristinamarilena95@gmail.com>
2019-03-14 12:02:49 +0100

    Adding the user name and surname to the accounts page

--- src/main/java/com/blueharvest/demo/DemoApplication.java
+++ src/main/java/com/blueharvest/demo/DemoApplication.java
@@ -10,12 +10,4 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    /**
-     * TODO Mockito test
-     * TODO unit test
-     * TODO service test
-     * TODO check jpa methods
-     * TODO integration test
-     */
-
 }
