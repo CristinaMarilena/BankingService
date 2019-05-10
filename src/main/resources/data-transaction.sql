f48d5fd87ec4fcbcd9e400db22ddac56ec8cd6fa
“CristinaMarilena” <cristinamarilena95@gmail.com>
2019-05-10 16:18:11 +0200

    Added configuration property files

---
+++ src/main/resources/data-transaction.sql
@@ -0,0 +1,2 @@
+INSERT INTO transaction(transaction_id, from_account_id, to_account_id, amount)
+            VALUES (1, 1, 2, 300);
---
+++ src/main/resources/data-user.sql
@@ -0,0 +1,2 @@
+INSERT INTO user(user_id, enabled, name, password, surname, username)
+            VALUES(1, 1, 'Harry', 'pass', 'Potter', 'harryP');
