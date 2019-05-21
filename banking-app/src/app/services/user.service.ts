2157f402c34865ac2507d7698893d6dc20ce38b2
“CristinaMarilena” <cristinamarilena95@gmail.com>
2019-05-21 16:59:45 +0200

    Updated tar version because of security vulerability

--- banking-app/src/app/services/user.service.ts
+++ banking-app/src/app/services/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { access } from 'fs';
 
 export interface UserAccountsInterface{
   username: string,
